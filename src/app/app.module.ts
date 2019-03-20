96c4b9863635bb0a34a2f3ebf518ea36da196f2b
Trigueiro Neto <neto.paladinus@gmail.com>
2019-03-20 03:37:09 -0300

    feat: implementa firebase como serviço temporário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
 import { MaterialConfigModule } from './core/modules/material-config.module';
+import { AppService } from './app.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
 
+        AngularFireModule.initializeApp(environment.firebase),
+
         MaterialConfigModule,
     ],
-    providers: [],
+    providers: [AppService, AngularFirestore],
     bootstrap: [AppComponent]
 })
 export class AppModule { }

7e4edfc4b6185940cfed95f941828810e307414d
Mateus Gomes De Morais <mgmorais@stefanini.com>
2019-05-02 16:10:37 -0300

    Finaliza a configuração de arquivos para execução do protractor

---
+++ e2e/talk.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { TalkDetailPage } from "./page-objects/talk-detail.po";
+import { browser } from 'protractor';
+const endereco = "http://localhost:4200"
+
+describe("Tela principal Feedback tool", () => {
+
+    let talk = new TalkDetailPage;
+
+    it("Deve criar um treinamento sem tópico", () => {
+
+    });
+
+    it("Deve criar um treinamento com tópicos", () => {
+    });
+
+    it("Deve remover um tópico na criação do treinamento", () =>{
+        
+    });
+
+});
---
+++ protractor.conf.js
@@ -0,0 +1,39 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 30000,
+  specs: [
+    './e2e/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': { 'args': [ "--disable-gpu", "log-level=2"] },
+    'loggingPrefs': { 
+      'driver': 'OFF',
+      'server': 'OFF',
+      'browser': 'OFF'
+    }
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 50000,
+    print: function() {}
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { 
+      displayStacktrace: true,
+      displayFailuresSummary: true,
+      displayFailuredSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true } }));
+  }
+};
